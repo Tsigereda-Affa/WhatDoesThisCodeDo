231550ef5bacc3206aa57bc2c41c21e65ad6a141
tsigereda-affa <affa16.a@gmail.com>
2018-07-16 15:27:05 -0400

    making update

--- src/Main.java
+++ src/Main.java
@@ -6,37 +6,37 @@ public class Main {
 //Clean up the following code by adding comments where appropriate.
 // If you read the program carefully you should be able to follow it. Also, rename variables and methods to be meaningful and consistent.
         Scanner keyboard = new Scanner(System.in);
-        double num1 = 0;
-        double num2= 0;
-        double num3 = 0;
+        double loan = 0;
+        double intrest= 0;
+        double monthlyPayment =0;
         double payment = 0;
-        int cp = 0;
-        int trm;
+        double balance = 0;
+        int term;
 
         System.out.printf("Enter the loan amount: ");
-        num1 = keyboard.nextDouble();
+        loan = keyboard.nextDouble();
 
 
         System.out.printf("Enter the interest rate on the loan: ");
-        num2 = keyboard.nextDouble();
+        intrest = keyboard.nextDouble();
 
         System.out.printf("Enter the term (years) for the loan payment: ");
-        trm = keyboard.nextInt();
+        term = keyboard.nextInt();
 
         System.out.printf("\n======================================");
         keyboard.close();
 
-        num3 = getMonthlyPayment(num1, num2, trm);
-        payment = -(num3*(trm*12));
-        System.out.format( "Balance owed to bank: ");
-        System.out.format( "Minimum monthly payment: ");
+        monthlyPayment= getMonthlyPayment(loan, intrest, term);
+        balance = -(monthlyPayment*(term*12));
+        System.out.format("%-30s$%-+10.2f%n", "Balance owed to bank: ", balance);
+        System.out.format("%-30s$%-+10.2f%n", "Minimum monthly payment: ",monthlyPayment );
     }
 
 
-    public static double getMonthlyPayment(double numl, double num2, int trm) {
-        double rate = (num2 / 100) / 12;
+    public static double getMonthlyPayment(double loan, double interst, int term) {
+        double rate = (interst / 100) / 12;
         double base = (rate + 1);
-        double months = trm * 12;
+        double months = term * 12;
         double result = 0.0;
         result = 1* (rate * (Math.pow(base, months)) / Math.pow(base, months));
 
